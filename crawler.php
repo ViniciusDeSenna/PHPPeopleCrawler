e9f3beeb52226c5260f1e99be5dfa698ab782fad
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2024-12-29 19:21:32 -0300

    crawler revisado

--- crawler.php
+++ crawler.php
@@ -1,72 +1,111 @@
 <?php
-$page = 0;
-$url = "https://people.php.net/";
-$havePeople = true;
-$nickNames = array();
-$peoples = array();
 
-// percorro enquanto tiver colaboradores na tela
-while($havePeople) {
-    $page++;
+// Função para buscar o conteúdo da página
+function getPageContent($url) {
+    $options = [
+        "http" => [
+            "method" => "GET",
+            "header" => "User-Agent: Mozilla/5.0"
+        ]
+    ];
+    $context = stream_context_create($options);
+    return file_get_contents($url, false, $context);
+}
 
-    $urlPage = $url . '?page=' . $page;
+// Função para extrair os nicknames da página de listagem
+function getNicknamesFromPage($url) {
+    $nicknames = [];
+    $content = getPageContent($url);
 
     $dom = new DOMDocument();
-    @$dom->loadHTML(file_get_contents($urlPage));
+    @$dom->loadHTML($content);
 
     $table = $dom->getElementsByTagName("tbody")->item(0);
     $links = $table->getElementsByTagName("a");
 
-    // Valida se ainda tem colaboradores na pagina, se não tiver finaliza a buca por nicknames
-    if ($links->length <= 0) {
-        $havePeople = false;
-        continue;
-    }
-
-    // Pego os nicknames
     foreach($links as $link) {
-        $nickNames[] = $link->textContent;
+        $nicknames[] = $link->textContent;
     }
 
-    unset($urlPage, $dom, $table, $links);
+    return $nicknames;
 }
 
-// Acesso a página de cada nickname e coleto os dados
-foreach($nickNames as $nick) {
+// Função para extrair os dados do perfil de cada colaborador
+function getProfileData($nickName, $url) {
+    $profile = [];
+    $content = getPageContent($url . $nickName);
+
     $dom = new DOMDocument();
-    @$dom->loadHTML(file_get_contents($url . $nick));
+    @$dom->loadHTML($content);
 
     $section = $dom->getElementsByTagName("section")->item(0);
 
+    // Recuperando a imagem do perfil
     $img = str_replace('?s=460', '', $section->getElementsByTagName("img")->item(0)->getAttribute('src'));
     if (!preg_match('#^https?:#', $img)) {
         $img = 'https:' . $img;
     }
+
     $name = $dom->getElementsByTagName("h1")->item(0)->textContent;
-    $nickName = $dom->getElementsByTagName("h2")->item(0)->textContent;
+    $nick = $dom->getElementsByTagName("h2")->item(0)->textContent;
 
     $ul = $section->getElementsByTagName("ul")->item(0);
     $li = $ul->getElementsByTagName("li")->item(0);
 
     $mail = trim($li->textContent);
 
-    $peoples[] = [
+    $profile = [
         "profileImg" => $img,
         "name" => $name,
-        "nickName" => $nickName,
+        "nickName" => $nick,
         "mail" => $mail,
     ];
 
-    // Baixo as imagens de perfil
-    $imagename= basename($img);
-    if(file_exists('./'.$imagename)){
-        continue;
-    } 
-    if(!file_exists("./imagens")){
-        mkdir("./imagens", 0777);
-    }
-    file_put_contents('./imagens/' . $nickName . '.jpg', file_get_contents($img)); 
+    return $profile;
 }
 
+// Função para baixar as imagens de perfil
+function downloadImage($img, $nickName) {
+    $imagename = basename($img);
+    $imagePath = './imagens/' . $nickName . '.jpg';
+    
+    if (!file_exists($imagePath)) {
+        if (!file_exists("./imagens")) {
+            mkdir("./imagens", 0777);
+        }
+        file_put_contents($imagePath, file_get_contents($img));
+    }
+}
+
+// URL base
+$url = "https://people.php.net/";
+$peoples = [];
+$page = 0;
+
+// Coletando os nicknames enquanto houver páginas
+do {
+    $page++;
+    $urlPage = $url . '?page=' . $page;
+
+    $nickNames = getNicknamesFromPage($urlPage);
+
+    if (empty($nickNames)) {
+        break;
+    }
+
+    // Para cada nickname, colete as informações
+    foreach($nickNames as $nick) {
+        $profileData = getProfileData($nick, $url);
+        $peoples[] = $profileData;
+
+        // Baixar a imagem do perfil
+        downloadImage($profileData['profileImg'], $nick);
+    }
+
+} while (!empty($nickNames));
+
+// Exibe os resultados
 var_dump($peoples);
 echo 'Programa rodado com sucesso, ' . $page . ' páginas percorridas e ' . count($peoples) . ' contribuintes encontrados!';
+
+?>
